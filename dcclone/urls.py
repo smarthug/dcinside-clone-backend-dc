734ed38d97018a1697e649a5cc1726cbb0bb8081
liradb2000 <liradb2000@live.co.kr>
2025-10-23 20:39:40 +0900

    Reorganize import statements for better clarity

--- dcclone/urls.py
+++ dcclone/urls.py
@@ -4,8 +4,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
-from core.views import GalleryViewSet, PostViewSet, CommentViewSet, hot_feed
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from core.views import GalleryViewSet, PostViewSet, CommentViewSet, hot_feed
 
 router = DefaultRouter()
 router.register(r'galleries', GalleryViewSet, basename='gallery')
