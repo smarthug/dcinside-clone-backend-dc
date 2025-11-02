19df263fc421de519b59f4dffa990a6c87c3371b
liradb2000 <liradb2000@live.co.kr>
2025-11-02 14:20:48 +0900

    refactor: prevent delete the post from the database;

--- core/models.py
+++ core/models.py
@@ -35,6 +35,8 @@ class Post(TimestampedModel):
     image = models.ImageField(upload_to='posts/', blank=True, null=True)
     recommend = models.IntegerField(default=0)
     views = models.PositiveIntegerField(default=0)
+    is_delete = models.BooleanField(default=False)
+    is_pending = models.BooleanField(default=False)
 
     def __str__(self):
         return f"[{self.gallery.slug}] {self.title}"
