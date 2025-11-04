2b066b4bde8f70a30ed239de3b8333ae40161062
smarthug <kirklayer@gmail.com>
2025-11-04 16:26:53 +0900

    fix: remove token parameter from user verification URL

--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,5 @@ from .views import UserProfileView, UserRegistrationView, UserVerifyView
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='user-register'),
     path('me/', UserProfileView.as_view(), name='user-profile'),
-    path('verify/<str:token>/', UserVerifyView.as_view(), name='user-verify'),
+    path('verify/', UserVerifyView.as_view(), name='user-verify'),
 ]
