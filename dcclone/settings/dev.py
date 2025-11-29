8eea68bc5dab3330b71a042de80127db477ae129
liradb2000 <liradb2000@live.co.kr>
2025-11-30 03:01:05 +0900

    Add Redis cache configuration to development settings

--- dcclone/settings/dev.py
+++ dcclone/settings/dev.py
@@ -1,3 +1,10 @@
 from .base import *  # noqa
 
 INSTALLED_APPS += ["django_extensions"]
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": os.getenv("REDIS_URL", "redis://redis:6379/1"),
+    }
+}
